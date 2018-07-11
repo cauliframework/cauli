5e0a3c56b53f1be4938fc2fe8b73f39a5bf0cf49
Cornelius Horstmann <site-github@brototyp.de>
2018-07-11 16:23:52 +0200

    Added Cauli Project

---
+++ Cauli/Cauli.h
@@ -0,0 +1,19 @@
+//
+//  Cauli.h
+//  Cauli
+//
+//  Created by Cornelius Horstmann on 11.07.18.
+//  Copyright © 2018 brototyp.de. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cauli.
+FOUNDATION_EXPORT double CauliVersionNumber;
+
+//! Project version string for Cauli.
+FOUNDATION_EXPORT const unsigned char CauliVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cauli/PublicHeader.h>
+
+
