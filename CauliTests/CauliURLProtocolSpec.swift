45cf40720705c4e7217ade4328f7b5b8c9c6b8f2
Cornelius Horstmann <cornelius@brototyp.de>
2018-10-08 16:56:10 +0200

    Fixed existing specs

--- CauliTests/CauliURLProtocolSpec.swift
+++ CauliTests/CauliURLProtocolSpec.swift
@@ -24,26 +24,27 @@
 import Foundation
 import Quick
 import Nimble
+import OHHTTPStubs
 
 class CauliURLProtocolSpec: QuickSpec {
     
-    let mocker = CauliURLProtocolDelegateMocker()
-    
     override func setUp() {
         super.setUp()
-        CauliURLProtocol.add(delegate: mocker)
+        // we are mocking everything
+        stub(condition: {_ in true}) { _ in
+            let stubData = "{\"response\":\"ok\"}".data(using: .utf8)
+            return OHHTTPStubsResponse(data: stubData!, statusCode:200, headers:["Content-Type":"application/json"])
+        }
         URLSessionConfiguration.cauliSwizzleDefaultSessionConfigurationGetter()
         URLProtocol.registerClass(CauliURLProtocol.self)
     }
     
     override func tearDown() {
         super.tearDown()
-        CauliURLProtocol.remove(delegate: mocker)
         URLProtocol.unregisterClass(CauliURLProtocol.self)
         URLSessionConfiguration.cauliSwizzleDefaultSessionConfigurationGetter()
     }
     
-    // TODO: add a mocking delegate/floret for all these requests
     override func spec() {
         describe("addDelegate") {
             it("calls the willRequest function on the delegate once a network request is performed") {
@@ -90,10 +91,6 @@ class CauliURLProtocolSpec: QuickSpec {
             }
         }
         describe("willRequest") {
-            // I am not to sure about this spec
-            // In the end it is just testing the mocked delegate
-            // I am not sure what would be the best way to test this implementation
-            // and have a mocked response (since we don't want to rely on the network)
             it("enables to change the designatedRequest") {
                 let delegate = CauliURLProtocolDelegateStub()
                 var didRespondRecord: Record?
@@ -105,26 +102,92 @@ class CauliURLProtocolSpec: QuickSpec {
                 }
                 
                 CauliURLProtocol.add(delegate: delegate)
-                // The mocker has do be added after the tested delegate
-                // otherwise it will not use the changed url
-                CauliURLProtocol.remove(delegate: self.mocker)
-                CauliURLProtocol.add(delegate: self.mocker)
                 let task = URLSession.shared.dataTask(with: URL(string: "https://cauli.works")!) { (data, response, error) in }
                 task.resume()
                 expect(didRespondRecord?.originalRequest.url?.absoluteString).toEventually(equal("https://cauli.works"))
                 expect(didRespondRecord?.designatedRequest.url?.absoluteString).toEventually(equal("https://cauli.works/modified"))
-                if case let .result(response)? = didRespondRecord?.result {
-                    expect(response.urlResponse.url?.absoluteString) == "https://cauli.works/modified"
-                } else {
-                    fail("Expected a result")
-                }
+                
+                expect {
+                    guard case let .result(response)? = didRespondRecord?.result else { return nil }
+                    return response.urlResponse.url?.absoluteString
+                    }.toEventually(equal("https://cauli.works/modified"))
             }
-            it("enables to set the result") {}
-            it("enables to set an error result") {}
+            it("enables to set the result") {
+                let delegate = CauliURLProtocolDelegateStub()
+                let expectedData = "spec_changed_response".data(using: .utf8)!
+                var didRespondRecord: Record?
+                delegate.willRequestClosure = { record in
+                    let urlResponse = URLResponse(url: record.designatedRequest.url!, mimeType: nil, expectedContentLength: expectedData.count, textEncodingName: nil)
+                    let response = Response(urlResponse, data: expectedData)
+                    record.result = .result(response)
+                }
+                delegate.didRespondClosure = { record in
+                    didRespondRecord = record
+                }
+                
+                CauliURLProtocol.add(delegate: delegate)
+                let task = URLSession.shared.dataTask(with: URL(string: "https://cauli.works")!) { (data, response, error) in }
+                task.resume()
+                expect {
+                    guard case let .result(response)? = didRespondRecord?.result else { return nil }
+                    return response.data
+                    }.toEventually(equal(expectedData))
+            }
+            it("enables to set an error result") {
+                let delegate = CauliURLProtocolDelegateStub()
+                let expectedError = NSError(domain: "domain", code: 0, userInfo: nil)
+                var didRespondRecord: Record?
+                delegate.willRequestClosure = { record in
+                    record.result = .error(expectedError)
+                }
+                delegate.didRespondClosure = { record in
+                    didRespondRecord = record
+                }
+                
+                CauliURLProtocol.add(delegate: delegate)
+                let task = URLSession.shared.dataTask(with: URL(string: "https://cauli.works")!) { (data, response, error) in }
+                task.resume()
+                expect {
+                    guard case let .error(error)? = didRespondRecord?.result else { return nil }
+                    return error
+                    }.toEventually(equal(expectedError))
+            }
         }
         describe("didRespond") {
-            it("calls with a correct result") {}
-            it("enables to update the result") {}
+            it("calls with a correct result") {
+                let delegate = CauliURLProtocolDelegateStub()
+                var didRespondRecord: Record?
+                delegate.didRespondClosure = { record in
+                    didRespondRecord = record
+                }
+                
+                CauliURLProtocol.add(delegate: delegate)
+                let task = URLSession.shared.dataTask(with: URL(string: "https://cauli.works")!) { (data, response, error) in }
+                task.resume()
+                let expectation: Expectation<Data?> = expect {
+                    guard case let .result(response)? = didRespondRecord?.result else { return nil }
+                    return response.data
+                    }
+                expectation.toEventuallyNot(beNil())
+            }
+            it("enables to update the result") {
+                let delegate = CauliURLProtocolDelegateStub()
+                let expectedData = "spec_changed_response".data(using: .utf8)!
+                var receivedData: Data?
+                
+                delegate.didRespondClosure = { record in
+                    let urlResponse = URLResponse(url: record.designatedRequest.url!, mimeType: nil, expectedContentLength: expectedData.count, textEncodingName: nil)
+                    let response = Response(urlResponse, data: expectedData)
+                    record.result = .result(response)
+                }
+                
+                CauliURLProtocol.add(delegate: delegate)
+                let task = URLSession.shared.dataTask(with: URL(string: "https://cauli.works")!) { (data, response, error) in
+                    receivedData = data
+                }
+                task.resume()
+                expect(receivedData).toEventually(equal(expectedData))
+            }
         }
     }
 }
