10a9e170bb76667a7c6b8cd95be4c6cb5d19cca7
Cornelius Horstmann <cornelius@brototyp.de>
2018-11-29 14:04:13 +0100

    Used the new mapping in the example application

--- Example/cauli-ios-example/cauli-ios-example/Cauli+Customized.swift
+++ Example/cauli-ios-example/cauli-ios-example/Cauli+Customized.swift
@@ -16,7 +16,18 @@ internal extension Cauli {
 //        return FindReplaceFloret(replacements: [sslUrl])
         return FindReplaceFloret(replacements: [])
     }()
-    static let mockFloret = MockFloret()
+    static let mockFloret: MockFloret = {
+        let floret = MockFloret()
+        floret.addMapping({ request, floret in
+            if let host = request.url?.host,
+                host == "invalidurl.invalid" {
+                return floret.resultForPath("2e018ce0e517c39e2717efb0151e3b65/97f6f69543dd9e669ee47a0cabbb40b8")
+            } else {
+                return nil
+            }
+        })
+        return floret
+    }()
     static let inspectorFloret = InspectorFloret()
     static let customShared = Cauli([anonymizeIPFloret, findReplaceFloret, mockFloret, inspectorFloret], configuration: Configuration.standard)
 }
