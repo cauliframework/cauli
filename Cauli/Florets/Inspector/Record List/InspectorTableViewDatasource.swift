6472ceb1ab3c36d30ba3ccf384dce72b85459751
Shukuyen <cornelius@schiffr.de>
2019-02-17 23:50:48 +0100

    Inlined private append function in InspectorTableViewDatasource

--- Cauli/Florets/Inspector/Record List/InspectorTableViewDatasource.swift
+++ Cauli/Florets/Inspector/Record List/InspectorTableViewDatasource.swift
@@ -74,17 +74,13 @@ internal class InspectorTableViewDatasource: NSObject {
         }
     }
 
-    private func append(records: [Record]) {
-        items += records
-    }
-    
     internal func append(records: [Record], to tableView: UITableView, completion: ((_ finished: Bool)->Void)? = nil) {
         performBatchUpdate(in: tableView, updates: {
             let numberOfExistingRecords = filteredItems.count
             let numberOfAddedRecords = filter(records: records).count
             let indexPaths = (numberOfExistingRecords..<(numberOfExistingRecords + numberOfAddedRecords)).map { IndexPath(row: $0, section: 0) }
             tableView.insertRows(at: indexPaths, with: .bottom)
-            append(records: records)
+            items += records
         }, completion: completion)
     }
 
