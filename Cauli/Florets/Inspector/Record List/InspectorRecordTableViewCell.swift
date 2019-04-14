4a143db8cf1bd5ad21e45de6e177eb6bc28decfa
Cornelius Schiffer <cornelius@schiffr.de>
2019-04-14 14:31:50 +0200

    #179 added remaining CFNetworkErrors and disabled swiftlint rules for this function/file

--- Cauli/Florets/Inspector/Record List/InspectorRecordTableViewCell.swift
+++ Cauli/Florets/Inspector/Record List/InspectorRecordTableViewCell.swift
@@ -22,6 +22,7 @@
 
 import UIKit
 
+// swiftlint:disable type_body_length
 internal class InspectorRecordTableViewCell: UITableViewCell {
 
     static let timeFormatter: DateFormatter = {
@@ -84,14 +85,16 @@ internal class InspectorRecordTableViewCell: UITableViewCell {
         }
     }
 
+    // swiftlint:disable cyclomatic_complexity
+    // swiftlint:disable function_body_length
     private func errorString(for code: Int) -> String {
         guard let error = CFNetworkErrors(rawValue: Int32(code)) else {
-            return "Error"
+            return "ERR"
         }
         switch error {
         case .cfHostErrorHostNotFound, .cfurlErrorCannotFindHost:
             return "Host not found"
-        case .cfHostErrorUnknown:
+        case .cfHostErrorUnknown, .cfurlErrorUnknown, .cfNetServiceErrorUnknown:
             return "Unknown error"
         case .cfftpErrorUnexpectedStatusCode:
             return "Unexpected status code"
@@ -137,8 +140,117 @@ internal class InspectorRecordTableViewCell: UITableViewCell {
             return "Auth required"
         case .cfurlErrorTimedOut, .cfNetServiceErrorTimeout:
             return "Timeout"
+        case .cfsocksErrorUnknownClientVersion:
+            return "Unknown client version"
+        case .cfsocksErrorUnsupportedServerVersion:
+            return "Unsupported server version"
+        case .cfsocks4ErrorRequestFailed:
+            return "Request failed"
+        case .cfsocks4ErrorIdentdFailed:
+            return "Ident failed"
+        case .cfsocks4ErrorIdConflict:
+            return "ID conflict"
+        case .cfsocks4ErrorUnknownStatusCode:
+            return "Unknown status code"
+        case .cfsocks5ErrorBadState:
+            return "Bad state"
+        case .cfsocks5ErrorBadResponseAddr:
+            return "Bad response address"
+        case .cfsocks5ErrorBadCredentials:
+            return "Bad credentials"
+        case .cfsocks5ErrorUnsupportedNegotiationMethod:
+            return "Unsupported negotiation method"
+        case .cfsocks5ErrorNoAcceptableMethod:
+            return "No acceptable method"
+        case .cfStreamErrorHTTPSProxyFailureUnexpectedResponseToCONNECTMethod:
+            return "HTTPS unexpected connect response"
+        case .cfurlErrorBackgroundSessionInUseByAnotherProcess:
+            return "Background session in use"
+        case .cfurlErrorBackgroundSessionWasDisconnected:
+            return "Background session disconnected"
+        case .cfurlErrorCancelled:
+            return "Cancelled"
+        case .cfurlErrorZeroByteResource:
+            return "Zero byte resource"
+        case .cfurlErrorCannotDecodeRawData:
+            return "Cannot decode raw data"
+        case .cfurlErrorCannotDecodeContentData:
+            return "Cannot decode content data"
+        case .cfurlErrorCannotParseResponse:
+            return "Cannot parse response"
+        case .cfurlErrorInternationalRoamingOff:
+            return "Roaming off"
+        case .cfurlErrorCallIsActive:
+            return "Call is active"
+        case .cfurlErrorDataNotAllowed:
+            return "Data not allowed"
+        case .cfurlErrorRequestBodyStreamExhausted:
+            return "Req body stream exhausted"
+        case .cfurlErrorAppTransportSecurityRequiresSecureConnection:
+            return "ATS requires secure connection"
+        case .cfurlErrorFileDoesNotExist:
+            return "File does not exist"
+        case .cfurlErrorFileIsDirectory:
+            return "File is directory"
+        case .cfurlErrorNoPermissionsToReadFile:
+            return "No read permission"
+        case .cfurlErrorDataLengthExceedsMaximum:
+            return "Data length exceed max"
+        case .cfurlErrorFileOutsideSafeArea:
+            return "File outside safe area"
+        case .cfurlErrorSecureConnectionFailed:
+            return "Secure connection failed"
+        case .cfurlErrorServerCertificateHasBadDate:
+            return "Bad server cert date"
+        case .cfurlErrorServerCertificateUntrusted:
+            return "Untrusted server cert"
+        case .cfurlErrorServerCertificateHasUnknownRoot:
+            return "Unknown server cert root"
+        case .cfurlErrorServerCertificateNotYetValid:
+            return "Server cert not yet valid"
+        case .cfurlErrorClientCertificateRejected:
+            return "Client cert rejected"
+        case .cfurlErrorClientCertificateRequired:
+            return "Client cert required"
+        case .cfurlErrorCannotLoadFromNetwork:
+            return "Cannot load from network"
+        case .cfurlErrorCannotCreateFile:
+            return "Cannot create file"
+        case .cfurlErrorCannotOpenFile:
+            return "Cannot open file"
+        case .cfurlErrorCannotCloseFile:
+            return "Cannot close file"
+        case .cfurlErrorCannotWriteToFile:
+            return "Cannot write to file"
+        case .cfurlErrorCannotRemoveFile:
+            return "Cannot remove file"
+        case .cfurlErrorCannotMoveFile:
+            return "Cannot move file"
+        case .cfurlErrorDownloadDecodingFailedMidStream:
+            return "Download decode failed mid stream"
+        case .cfurlErrorDownloadDecodingFailedToComplete:
+            return "Download decode failed to complete"
+        case .cfhttpCookieCannotParseCookieFile:
+            return "Cannot parse cookie file"
+        case .cfNetServiceErrorCollision:
+            return "Collision"
+        case .cfNetServiceErrorNotFound:
+            return "Not found"
+        case .cfNetServiceErrorInProgress:
+            return "In Progress"
+        case .cfNetServiceErrorBadArgument:
+            return "Bad Argument"
+        case .cfNetServiceErrorCancel:
+            return "Cancel"
+        case .cfNetServiceErrorInvalid:
+            return "Invalid"
+        case .cfNetServiceErrorDNSServiceFailure:
+            return "DNS service failure"
         default:
-            return "Error"
+            return "ERR"
         }
     }
+    // swiftlint:enable cyclomatic_complexity
+    // swiftlint:enable function_body_length
 }
+// swiftlint:enable type_body_length
